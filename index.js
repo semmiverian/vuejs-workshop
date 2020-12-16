955321db4f1192776c99a7a30cf4fae85eac16bb
Semmiverian <semmivp1@gmail.com>
2020-12-16 21:59:47 +0700

    finish

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0
+      counter: 0,
+      newTodo: '',
+      todos: [],
+      users: [],
+      staticUsers: [],
+      findUser: ''
     }
+  },
+  methods: {
+    decrementCounter() {
+      this.counter--
+    },
+    incrementCounter() {
+      this.counter++
+    },
+    addTodo() {
+      this.todos.push(this.newTodo)
+    },
+    deleteTodo(selectedIndex) {
+      this.todos = this.todos.filter((todo, index) => index !== selectedIndex)
+    }
+  },
+  computed: {
+    todosCount() {
+      return this.todos.length
+    }
+  },
+  watch: {
+    findUser(value) {
+      if (value === '') {
+        this.users = this.staticUsers
+      } else {
+        this.users = this.staticUsers.filter(
+          (user) =>
+            user.name.first.toLowerCase().includes(value.toLowerCase()) ||
+            user.name.last.toLowerCase().includes(value.toLowerCase())
+        )
+      }
+    }
+  },
+  async mounted() {
+    const data = await fetch('https://randomuser.me/api/?results=10').then((res) => res.json())
+    this.users = data.results
+    this.staticUsers = data.results
   }
 })
 
